39d6ecd9cb3c567d6b74327f2fd2d1638e1f652b
Kennedy Eruba <erubakenny@gmail.com>
2021-06-18 22:11:31 +0000

    Basic setup

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Navigation.scss';
+
+const Navigation = () => (
+  <div className="Navigation">
+    Navigation Component
+  </div>
+);
+
+Navigation.propTypes = {};
+
+Navigation.defaultProps = {};
+
+export default Navigation;
